840c6001e1c93a7107b1327692b3eeca3c6e1aa3
Tahmid Azam <12TAzam@mcsoxford.org>
2022-12-15 13:18:24 +0000

    Adding horizontal scrolling to plot visualisation.

--- Trace/macOS/DocumentView Subviews/ContentView subviews/PlotView.swift
+++ Trace/macOS/DocumentView Subviews/ContentView subviews/PlotView.swift
@@ -12,6 +12,11 @@ struct PlotView: View {
     @Binding var doc: TraceDocument
     
     @State var chartHeightScaleFactor = 1.0
+    @State var chartWidthScaleFactor = 1.0
+    
+    @GestureState var scale: CGFloat = 1.0
+    
+    var electrodeColumnSize: CGFloat = 100.0
     
     var sortedStreams: [Stream] {
         return doc.contents.streams.sorted { lhs, rhs in
@@ -24,24 +29,94 @@ struct PlotView: View {
     }
     
     var body: some View {
-        Group {
+        VStack(spacing: 0.0) {
+            plot
+            
+            Divider()
+            
+            HStack {
+                Text(doc.contents.formattedSampleCount ?? "")
+            }
+            .padding()
+        }
+            .toolbar {
+                ToolbarItemGroup {
+                    Label("Vertical chart scale", systemImage: "arrow.up.and.line.horizontal.and.arrow.down")
+                    
+                    chartHeightScaleFactorPicker
+                    
+                    Label("Horizontal chart scale", systemImage: "arrow.left.and.line.vertical.and.arrow.right")
+                    
+                    chartWidthScaleFactorPicker
+                }
+            }
+    }
+    
+    var chartHeightScaleFactorPicker: some View {
+        Picker(selection: $chartHeightScaleFactor) {
+            ForEach([0.5, 0.75, 1.0, 2.0, 3.0, 4.0, 5.0], id: \.self) { scaleFactor in
+                Text(scaleFactor, format: .percent)
+            }
+        } label: {
+            Label("Vertical chart scale", systemImage: "arrow.up.and.line.horizontal.and.arrow.down")
+        }
+        
+    }
+    
+    var chartWidthScaleFactorPicker: some View {
+        Picker(selection: $chartWidthScaleFactor) {
+            ForEach([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0], id: \.self) { scaleFactor in
+                Text(scaleFactor, format: .percent)
+            }
+        } label: {
+            Label("Horizontal chart scale", systemImage: "arrow.left.and.line.vertical.and.arrow.right")
+        }
+    }
+    
+    var plot: some View {
+        HStack(spacing: 0.0) {
             GeometryReader { proxy in
-                let chartHeight = Double(proxy.size.height) / Double(doc.contents.streams.count)
+                let rowHeight = Double(proxy.size.height) / Double(doc.contents.streams.count)
                 
                 ZStack {
-                    ForEach(sortedStreams.indices, id: \.self) { streamIndex in
-                        let stream = sortedStreams[streamIndex]
-                        
-                        let x = (proxy.size.width - 50) / 2
-                        let y = (chartHeight / 2) + Double(streamIndex) * chartHeight
-                        
-                        HStack(alignment: .center) {
-                            Text(stream.electrode.symbol)
-                                .frame(width: 50, alignment: .trailing)
-
+                    VStack(spacing: 0.0) {
+                        ForEach(sortedStreams.indices, id: \.self) { streamIndex in
+                            let stream = sortedStreams[streamIndex]
+                            
+                            HStack(alignment: .firstTextBaseline, content: {
+                                Text("\(streamIndex + 1)")
+                                    .font(.caption)
+                                    .foregroundColor(.secondary)
+                                
+                                Spacer()
+                                
+                                Text(stream.electrode.symbol)
+                            })
+                                .padding(.horizontal)
+                                .frame(width: electrodeColumnSize, height: rowHeight, alignment: .trailing)
+                        }
+                    }
+                }
+            }
+            .padding(.vertical)
+            .background(Color(.textBackgroundColor))
+            .frame(width: electrodeColumnSize)
+            
+            Divider()
+            
+            GeometryReader { proxy in
+                let rowHeight = Double(proxy.size.height) / Double(doc.contents.streams.count)
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    ZStack {
+                        ForEach(sortedStreams.indices, id: \.self) { streamIndex in
                             if let potentialRange = doc.contents.potentialRange {
                                 let yLimit = max(abs(potentialRange.lowerBound), abs(potentialRange.upperBound))
-                                
+
+                                let stream = sortedStreams[streamIndex]
+
+                                let y = (rowHeight / 2) + Double(streamIndex) * rowHeight
+
                                 Chart(TraceDocumentContents.sampleDataPoints(from: [stream], sampleRate: doc.contents.sampleRate)) { point in
                                     LineMark(
                                         x: .value("time/s", point.timestamp),
@@ -57,26 +132,14 @@ struct PlotView: View {
                                 })
                                 .chartXAxis(.hidden)
                                 .chartLegend(.hidden)
+                                .frame(width: (proxy.size.width) * chartWidthScaleFactor, height: rowHeight * chartHeightScaleFactor)
+                                .position(x: ((proxy.size.width) * chartWidthScaleFactor) / 2, y: y)
                             }
                         }
-                        .frame(height: chartHeight * chartHeightScaleFactor)
-                        .position(x: x, y: y)
                     }
                 }
             }
-            .padding()
-        }
-        .toolbar {
-            ToolbarItem {
-                Picker(selection: $chartHeightScaleFactor) {
-                    ForEach([0.5, 0.75, 1.0, 2.0, 3.0, 4.0, 5.0], id: \.self) { scaleFactor in
-                        Text(scaleFactor, format: .percent)
-                    }
-                } label: {
-                    Text("Zoom")
-                }
-
-            }
+            .padding(.vertical)
         }
     }
 }
